203610613e873db2e035d61026cec7c825fcf80c
opsigor <igorpsant95@gmail.com>
2015-09-09 08:19:37 -0300

    Fix no UnionFind

--- UnionFind.js
+++ UnionFind.js
@@ -1,43 +1,43 @@
-module.exports = function(){
+module.exports = function() {
   return {
     Conjuntos: {},
-    MakeSet: function(v1){
+    Find: function(v1) {
+      return v1.conjunto;
+    },
+    MakeSet: function(v1) {
       this.Conjuntos[v1.identificador] = {
         vertices: [v1],
         quantity: 1
       }
       v1.conjunto = v1.identificador;
     },
-    Union: function(v1,v2){
-      var conjuntoDoVerticeV1 = this.Find(v1)
-      ,   conjuntoDoVerticeV2 = this.Find(v2)
-      ,   conjuntoInteiro1 = this.Conjuntos[conjuntoDoVerticeV1]
-      ,   conjuntoInteiro2 = this.Conjuntos[conjuntoDoVerticeV2];
+    Union: function(v1, v2) {
+      var nomeDoPrimeiroConjunto = this.Find(v1),
+        nomeDoSegundoConjunto = this.Find(v2),
+        conjuntoInteiro1 = this.Conjuntos[nomeDoPrimeiroConjunto],
+        conjuntoInteiro2 = this.Conjuntos[nomeDoSegundoConjunto];
 
-      if(conjuntoDoVerticeV1 == conjuntoDoVerticeV2){
+      if (nomeDoPrimeiroConjunto == nomeDoSegundoConjunto) {
         return false;
       }
-      if(conjuntoInteiro1.quantity < conjuntoInteiro2.quantity){
-        conjuntoInteiro1.vertices.map(function(value){
-          value.conjunto = conjuntoDoVerticeV2;
+      if (conjuntoInteiro1.quantity < conjuntoInteiro2.quantity) {
+        conjuntoInteiro1.vertices.map(function(value) {
+          value.conjunto = nomeDoSegundoConjunto;
           return value;
         })
-        conjuntoInteiro2.vertices.concat(conjuntoInteiro1.vertices);
+        conjuntoInteiro2.vertices = conjuntoInteiro2.vertices.concat(conjuntoInteiro1.vertices);
         conjuntoInteiro2.quantity += conjuntoInteiro1.quantity;
-        delete this.Conjuntos[conjuntoInteiro1];
+        delete this.Conjuntos[nomeDoPrimeiroConjunto];
       } else {
-        conjuntoInteiro2.vertices.map(function(value){
-  				value.conjunto = conjuntoDoVerticeV1;
-  				return value;
-  			})
-  			conjuntoInteiro1.vertices.concat(conjuntoInteiro2.vertices)
-  			conjuntoInteiro1.quantity += conjuntoInteiro2.quantity;
-  			delete this.Conjuntos[conjuntoInteiro2];
+        conjuntoInteiro2.vertices.map(function(value) {
+          value.conjunto = nomeDoPrimeiroConjunto;
+          return value;
+        })
+        conjuntoInteiro1.vertices = conjuntoInteiro1.vertices.concat(conjuntoInteiro2.vertices)
+        conjuntoInteiro1.quantity += conjuntoInteiro2.quantity;
+        delete this.Conjuntos[nomeDoSegundoConjunto];
       }
       return true;
-    },
-    Find: function(v1){
-      return v1.conjunto;
     }
   }
 }
--- uri1552.js
+++ uri1552.js
@@ -1,66 +1,75 @@
-(function () {
-  var fs = require('fs')
-  ,   Grafo = require('./Grafo')
-  ,   Vertice = require('./Vertice')
-  ,   UnionFind = require('./UnionFind')();
-  fs.readFile('./entry.txt',{encoding: 'utf8'},callback);
-
-  function callback(err,data){
-    var content = data.split('\n')
-    ,   numberOfCases = content[0]
-    ,   cases = {}
-    ,   counter = 0
+(function() {
+  var fs = require('fs'),
+    Grafo = require('./Grafo'),
+    Vertice = require('./Vertice'),
+    UnionFind = require('./UnionFind')();
+  fs.readFile('./entry.txt', {
+    encoding: 'utf8'
+  }, callback);
+  // Função principal do app.
+  function callback(err, data) {
+    var content = data.split('\n'),
+      numberOfCases = content[0],
+      cases = {},
+      counter = 0;
     content.shift();
-
-    for(var i = 0; i < content.length;i++ ){
-      if(content[i].split(' ').length == 1){
+    // Lê o arquivo e cria os vértices.
+    for (var i = 0; i < content.length; i++) {
+      if (content[i].split(' ').length == 1) {
         counter++;
-        cases['case'+counter] = new Grafo();
-      } else if(content[i].split(' ').length > 1){
-        cases['case'+counter].addVertice(new Vertice(parseFloat(content[i].split(' ')[0]),parseFloat(content[i].split(' ')[1])));
+        if (i != content.length - 1) {
+          cases['case' + counter] = new Grafo();
+        }
+      } else if (content[i].split(' ').length > 1) {
+        cases['case' + counter]
+          .addVertice(new Vertice(parseFloat(content[i].split(' ')[0]), parseFloat(content[i].split(' ')[1])));
       }
     }
-    delete cases['case'+counter];
-    for(var key in cases) if(cases.hasOwnProperty(key)){
-      criaTodasArestas(cases[key]);
-      kruskal(cases[key])
-    }
+
+    for (var key in cases)
+      if (cases.hasOwnProperty(key)) {
+        // Pra cada case, cria toda as arestas
+        criaTodasArestas(cases[key]);
+        // Executa o kruskal
+        kruskal(cases[key])
+      }
   }
 
-  function getDistance(v1,v2){
+  function getDistance(v1, v2) {
     var a = v1.x - v2.x,
-				b = v1.y - v2.y;
-		return Math.sqrt((a*a)+(b*b));
+      b = v1.y - v2.y;
+    return Math.sqrt((a * a) + (b * b));
   }
 
-  function criaTodasArestas(case_){
-    case_.vertices.forEach(function(value){
+  function criaTodasArestas(case_) {
+    case_.vertices.forEach(function(value) {
       UnionFind.MakeSet(value);
-      case_.vertices.forEach(function(obj){
-        if(obj.identificador != value.identificador){
-          case_.addAresta(value,obj,getDistance(value,obj));
+      case_.vertices.forEach(function(obj) {
+        if (obj.identificador != value.identificador) {
+          case_.addAresta(value, obj, getDistance(value, obj));
         }
       })
     })
   }
 
-  function kruskal(grafo){
-    function sortArestas(aresta1,aresta2){
+  function kruskal(grafo) {
+    function sortArestas(aresta1, aresta2) {
       return aresta1.distancia - aresta2.distancia;
     }
-    var mst = []
-    ,   floresta = grafo.getVertices()
-    ,   arestasOrdenadas = grafo.getArestas().sort(sortArestas).reverse();
-    arestasOrdenadas.forEach(function(value){
-      if(UnionFind.Find(value.de) != UnionFind.Find(value.para)){
+    var mst = [],
+      floresta = grafo.getVertices(),
+      arestasOrdenadas = grafo.getArestas().sort(sortArestas).reverse();
+    arestasOrdenadas.forEach(function(value) {
+      if (UnionFind.Find(value.de) != UnionFind.Find(value.para)) {
         mst.push(value);
-        UnionFind.Union(value.de,value.para);
+        UnionFind.Union(value.de, value.para);
       }
     })
     var somaTotal = 0;
-    mst.forEach(function(oi){
-      somaTotal += oi.distancia;
+    mst.forEach(function(data) {
+      console.log('DE: (x:' + data.de.x + ',' + data.de.y + ') | PARA:(' + data.para.x + ',' + data.para.y + ') | DISTANCIA:' + data.distancia);
+      somaTotal += data.distancia;
     })
-    console.log((somaTotal/100).toFixed(2));
+    console.log((somaTotal / 100).toFixed(2));
   }
 })();
